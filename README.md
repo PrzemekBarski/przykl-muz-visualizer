a964fc8e86940a40468376a0988e39a7891b5026
Przemysław Barski <barski.przemyslaw@gmail.com>
2025-02-20 13:58:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Audio waveform visualizer
+
+## Prerequisites
+
+- Node v22.7.0
+
+
+## Development:
+
+```
+$ npm start
+```
+
+Website will be served on http://localhost:8080/.
+
+## Build:
+
+```
+$ npm build
+```
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  entry: [
+    path.resolve(__dirname, "src/index.js")
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist",
+    filename: "bundle.js"
+  },
+  devServer: {
+    static: "."
+  },
+  watch: true
+}
